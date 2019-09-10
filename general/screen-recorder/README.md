c00acfe038184591a4e3288cc1a1c71fc8a1713d
x4nth055 <fullclip@protonmail.com>
2019-09-10 17:15:34 +0200

    added screen recorder tutorial

---
+++ general/screen-recorder/README.md
@@ -0,0 +1,4 @@
+# [How to Make a Screen Recorder in Python](https://www.thepythoncode.com/article/make-screen-recorder-python)
+To run this:
+- `pip3 install -r requirements.txt`
+- `python3 screen_recorder.py`
---
+++ general/screen-recorder/screen_recorder.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+import pyautogui
+
+# display screen resolution, get it from your OS settings
+SCREEN_SIZE = (1920, 1080)
+# define the codec
+fourcc = cv2.VideoWriter_fourcc(*"XVID")
+# create the video write object
+out = cv2.VideoWriter("output.avi", fourcc, 20.0, (SCREEN_SIZE))
+
+while True:
+    # make a screenshot
+    img = pyautogui.screenshot()
+    # convert these pixels to a proper numpy array to work with OpenCV
+    frame = np.array(img)
+    # convert colors from BGR to RGB
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # write the frame
+    out.write(frame)
+    # show the frame
+    cv2.imshow("screenshot", frame)
+    # if the user clicks q, it exits
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+# make sure everything is closed when exited
+cv2.destroyAllWindows()
+out.release()
+
