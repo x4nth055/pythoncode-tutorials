b4988ee6d3fc5f3d7337f4ca2eced82ca2c21169
x4nth055 <fullclip@protonmail.com>
2019-09-27 00:19:30 +0200

    add python code generator in machine-learning/nlp/text-generator

--- machine-learning/nlp/text-generator/README.md
+++ machine-learning/nlp/text-generator/README.md
@@ -1,32 +1,39 @@
 # [How to Build a Text Generator using Keras in Python](https://www.thepythoncode.com/article/text-generation-keras-python)
 To run this:
 - `pip3 install -r requirements.txt`
-- To use pre-trained text generator model that was trained on Alice's wonderland text book:
+- To use pre-trained text generator model that was trained on Alice's wonderland text book or Python Code:
     ```
     python generate.py --help
     ```
-    **Output:**
+    This will prompt you with the choice, seed and number of characters you want!
+    Here is an example on Alice's wonderland with 200 characters:
     ```
-    usage: generate.py [-h] [-n N_CHARS] seed
-
-    Text generator that was trained on Alice's Adventures in the Wonderland book.
-
-    positional arguments:
-    seed                  Seed text to start with, can be any english text, but
-                            it's preferable you take from the book itself.
-
-    optional arguments:
-    -h, --help            show this help message and exit
-    -n N_CHARS, --n-chars N_CHARS
-                            Number of characters to generate, default is 200.
-    ```
-    Generating 200 characters with that seed:
-    ```
-    python generate.py "down down down there was nothing else to do so alice soon began talking again " -n 200
-    ```
-    **Output:**
-    ```
-    Generating text: 100%|█████████████████████████████████████████████████████████████████████████████████████████████████| 200/200 [00:40<00:00,  5.02it/s]
     Generated text:
     the duchess asked to the dormouse she wanted about for the world all her life i dont know what to think that it was so much sort of mine for the world a little like a stalking and was going to the mou
+    ```
+    Another example:
+    ```
+    Please choose which model you want to generate text with:
+    1 - Alice's wonderland
+    2 - Python Code
+    2
+    Enter the seed, enter q to quit, maximum 100 characters:
+    import os
+    import sys
+    import subprocess
+    import numpy as np
+    q
+    Enter number of characters you want to generate: 200
+    Generating text: 100%|█████████████████████████████████████████████████████████████████████████████████████████████████| 200/200 [00:44<00:00,  4.68it/s]
+    Generated text:
+    import pandas as pd
+    import os
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import tensorflow as tf
+
+    config = tf.configproto(intra_op_parallelism_threads=n
+
     ```
--- machine-learning/nlp/text-generator/generate.py
+++ machine-learning/nlp/text-generator/generate.py
@@ -5,10 +5,22 @@ from keras.models import Sequential
 from keras.layers import Dense, LSTM, Dropout, Activation
 from keras.callbacks import ModelCheckpoint
 
-# seed = "do not try to"
 
-char2int = pickle.load(open("data/wonderland-char2int.pickle", "rb"))
-int2char = pickle.load(open("data/wonderland-int2char.pickle", "rb"))
+
+message = """
+Please choose which model you want to generate text with:
+1 - Alice's wonderland
+2 - Python Code
+"""
+choice = int(input(message))
+assert choice == 1 or choice == 2
+
+if choice == 1:
+    char2int = pickle.load(open("data/wonderland-char2int.pickle", "rb"))
+    int2char = pickle.load(open("data/wonderland-int2char.pickle", "rb"))
+elif choice == 2:
+    char2int = pickle.load(open("data/python-char2int.pickle", "rb"))
+    int2char = pickle.load(open("data/python-int2char.pickle", "rb"))
 
 sequence_length = 100
 n_unique_chars = len(char2int)
@@ -21,35 +33,38 @@ model = Sequential([
     Dense(n_unique_chars, activation="softmax"),
 ])
 
-model.load_weights("results/wonderland-v2-0.75.h5")
+if choice == 1:
+    model.load_weights("results/wonderland-v2-0.75.h5")
+elif choice == 2:
+    model.load_weights("results/python-v2-0.30.h5")
 
-if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser(description="Text generator that was trained on Alice's Adventures in the Wonderland book.")
-    parser.add_argument("seed", help="Seed text to start with, can be any english text, but it's preferable you take from the book itself.")
-    parser.add_argument("-n", "--n-chars", type=int, dest="n_chars", help="Number of characters to generate, default is 200.", default=200)
-    args = parser.parse_args()
+seed = ""
+print("Enter the seed, enter q to quit, maximum 100 characters:")
+while True:
+    result = input("")
+    if result.lower() == "q":
+        break
+    seed += f"{result}\n"
+seed = seed.lower()
+n_chars = int(input("Enter number of characters you want to generate: "))
 
-    n_chars = args.n_chars
-    seed = args.seed
+# generate 400 characters
+generated = ""
+for i in tqdm.tqdm(range(n_chars), "Generating text"):
+    # make the input sequence
+    X = np.zeros((1, sequence_length, n_unique_chars))
+    for t, char in enumerate(seed):
+        X[0, (sequence_length - len(seed)) + t, char2int[char]] = 1
+    # predict the next character
+    predicted = model.predict(X, verbose=0)[0]
+    # converting the vector to an integer
+    next_index = np.argmax(predicted)
+    # converting the integer to a character
+    next_char = int2char[next_index]
+    # add the character to results
+    generated += next_char
+    # shift seed and the predicted character
+    seed = seed[1:] + next_char
 
-    # generate 400 characters
-    generated = ""
-    for i in tqdm.tqdm(range(n_chars), "Generating text"):
-        # make the input sequence
-        X = np.zeros((1, sequence_length, n_unique_chars))
-        for t, char in enumerate(seed):
-            X[0, (sequence_length - len(seed)) + t, char2int[char]] = 1
-        # predict the next character
-        predicted = model.predict(X, verbose=0)[0]
-        # converting the vector to an integer
-        next_index = np.argmax(predicted)
-        # converting the integer to a character
-        next_char = int2char[next_index]
-        # add the character to results
-        generated += next_char
-        # shift seed and the predicted character
-        seed = seed[1:] + next_char
-
-    print("Generated text:")
-    print(generated)
+print("Generated text:")
+print(generated)
