db0b45ce83346825bfd6491b7d36d2aabf15a9a8
x4nth055 <fullclip@protonmail.com>
2020-06-03 12:19:41 +0200

    edited get hardware/sys information tutorial

--- general/sys-info/sys_info.py
+++ general/sys-info/sys_info.py
@@ -108,3 +108,32 @@ print(f"Total Bytes Sent: {get_size(net_io.bytes_sent)}")
 print(f"Total Bytes Received: {get_size(net_io.bytes_recv)}")
 
 
+# GPU information
+import GPUtil
+from tabulate import tabulate
+print("="*40, "GPU Details", "="*40)
+gpus = GPUtil.getGPUs()
+list_gpus = []
+for gpu in gpus:
+    # get the GPU id
+    gpu_id = gpu.id
+    # name of GPU
+    gpu_name = gpu.name
+    # get % percentage of GPU usage of that GPU
+    gpu_load = f"{gpu.load*100}%"
+    # get free memory in MB format
+    gpu_free_memory = f"{gpu.memoryFree}MB"
+    # get used memory
+    gpu_used_memory = f"{gpu.memoryUsed}MB"
+    # get total memory
+    gpu_total_memory = f"{gpu.memoryTotal}MB"
+    # get GPU temperature in Celsius
+    gpu_temperature = f"{gpu.temperature} °C"
+    gpu_uuid = gpu.uuid
+    list_gpus.append((
+        gpu_id, gpu_name, gpu_load, gpu_free_memory, gpu_used_memory,
+        gpu_total_memory, gpu_temperature, gpu_uuid
+    ))
+
+print(tabulate(list_gpus, headers=("id", "name", "load", "free memory", "used memory", "total memory",
+                                   "temperature", "uuid")))
