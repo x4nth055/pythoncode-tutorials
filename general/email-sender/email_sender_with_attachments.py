8453674710311a3417248acd1003f18004f48f7c
x4nth055 <fullclip@protonmail.com>
2020-09-08 16:48:56 +0200

    update sending emails tutorial

--- general/email-sender/email_sender_with_attachments.py
+++ general/email-sender/email_sender_with_attachments.py
@@ -1,7 +1,17 @@
 import smtplib
+from email import encoders
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
 from bs4 import BeautifulSoup as bs
+
+# list of files to send as an attachment to the email
+# feel free to add your attachments here
+files_to_send = [
+    "test.txt",
+    "1810.04805.pdf",
+]
+
 
 def send_mail(email, password, FROM, TO, msg):
     # initialize the SMTP server
@@ -35,12 +45,7 @@ msg["To"] = TO
 # set the subject
 msg["Subject"] = subject
 # set the body of the email as HTML
-html = """
-This email is sent using <b>Python </b>!
-"""
-# uncomment below line if you want to use the HTML template
-# located in mail.html
-# html = open("mail.html").read()
+html = open("mail.html").read()
 # make the text version of the HTML
 text = bs(html, "html.parser").text
 
@@ -50,6 +55,20 @@ html_part = MIMEText(html, "html")
 # attach the plain text version first
 msg.attach(text_part)
 msg.attach(html_part)
+
+for file in files_to_send:
+    # open the file as read in bytes
+    with open(file, "rb") as f:
+        # read the file content
+        data = f.read()
+        # create the attachement
+        attach_part = MIMEBase("application", "octet-stream")
+        attach_part.set_payload(data)
+    # encode the data to base 64
+    encoders.encode_base64(attach_part)
+    # add the Content-Disposition header
+    attach_part.add_header("Content-Disposition", f"attachment; filename= {file}")
+    msg.attach(attach_part)
 
 # send the mail
 send_mail(email, password, FROM, TO, msg)
