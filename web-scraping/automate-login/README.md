02916f715f3d310833f694d6ad822a4adeee57eb
x4nth055 <fullclip@protonmail.com>
2020-11-07 10:59:36 +0100

    add automate login using selenium tutorial

---
+++ web-scraping/automate-login/README.md
@@ -0,0 +1,5 @@
+# [How to Automate Login using Selenium in Python](https://www.thepythoncode.com/article/automate-login-to-websites-using-selenium-in-python)
+To run this:
+- `pip3 install -r requirements.txt`
+- Get [ChromeDriver](https://sites.google.com/a/chromium.org/chromedriver/home) and put it in the current directory.
+- Edit credentials in `automate_login.py` and run it!
---
+++ web-scraping/automate-login/automate_login.py
@@ -0,0 +1,36 @@
+from logging import error
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+
+# Github credentials
+username = "username"
+password = "password"
+
+# initialize the Chrome driver
+driver = webdriver.Chrome(r"C:\Users\STRIX\Desktop\selenium\chromedriver")
+# head to github login page
+driver.get("https://github.com/login")
+# find username/email field and send the username itself to the input field
+driver.find_element_by_id("login_field").send_keys(username)
+# find password input field and insert password as well
+driver.find_element_by_id("password").send_keys(password)
+# click login button
+driver.find_element_by_name("commit").click()
+# wait the ready state to be complete
+WebDriverWait(driver=driver, timeout=10).until(
+    lambda x: x.execute_script("return document.readyState === 'complete'")
+)
+error_message = "Incorrect username or password."
+# get the errors (if there are)
+errors = driver.find_elements_by_class_name("flash-error")
+# print the errors optionally
+# for e in errors:
+#     print(e.text)
+# if we find that error message within errors, then login is failed
+if any(error_message in e.text for e in errors):
+    print("[!] Login failed")
+else:
+    print("[+] Login successful")
+
+# close the driver
+driver.close()
