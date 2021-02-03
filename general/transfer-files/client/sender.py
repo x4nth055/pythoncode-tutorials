9243242a87848b6f936d8aa2e5789e88363a2e9b
x4nth055 <fullclip@protonmail.com>
2021-02-03 18:14:47 +0100

    fix progress bar reset in the end on transfering files tutorial

--- general/transfer-files/client/sender.py
+++ general/transfer-files/client/sender.py
@@ -26,7 +26,7 @@ def send_file(filename, host, port):
     # start sending the file
     progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
     with open(filename, "rb") as f:
-        for _ in progress:
+        while True:
             # read the bytes from the file
             bytes_read = f.read(BUFFER_SIZE)
             if not bytes_read:
@@ -47,7 +47,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Simple File Sender")
     parser.add_argument("file", help="File name to send")
     parser.add_argument("host", help="The host/IP address of the receiver")
-    parser.add_argument("-p", "--port", help="Port to use, default is 5001", default=5001)
+    parser.add_argument("-p", "--port", help="Port to use, default is 5001", type=int, default=5001)
     args = parser.parse_args()
     filename = args.file
     host = args.host
--- general/transfer-files/server/receiver.py
+++ general/transfer-files/server/receiver.py
@@ -41,7 +41,7 @@ filesize = int(filesize)
 # and writing to the file stream
 progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
 with open(filename, "wb") as f:
-    for _ in progress:
+    while True:
         # read 1024 bytes from the socket (receive)
         bytes_read = client_socket.recv(BUFFER_SIZE)
         if not bytes_read:    
