c33afeafd13d255a2f7999adb67c4ef4e3b4d1d9
Arne Schwarck <arneschwarck@gmail.com>
2021-02-24 11:30:15 +0100

    no error when search returns no results

--- general/gmail-api/mark_emails.py
+++ general/gmail-api/mark_emails.py
@@ -2,6 +2,33 @@ from common import gmail_authenticate, search_messages
 
 def mark_as_read(service, query):
     messages_to_mark = search_messages(service, query)
+    if len(messages_to_mark) == 0: # No emails found
+        return print("No emails found")
+    else:
+        print("="*50)
+        for message_id in messages_to_mark:
+            msg = service.users().messages().get(userId='me', id=message_id['id'], format='full').execute()
+            payload = msg['payload']
+            headers = payload.get("headers")
+            if headers:
+                # this section prints email basic info & creates a folder for the email
+                for header in headers:
+                    name = header.get("name")
+                    value = header.get("value")
+                    if name == 'From':
+                        # we print the From address
+                        print("From:", value)
+                    if name == "To":
+                        # we print the To address
+                        print("To:", value)
+                    if name == "Subject":
+                        # we print the Subject
+                        print("Subject:", value)
+                    if name == "Date":
+                        # we print the date when the message was sent
+                        print("Date:", value)
+            print("="*50)
+            print("MARKED AS READ")
     return service.users().messages().batchModify(
       userId='me',
       body={
@@ -12,6 +39,33 @@ def mark_as_read(service, query):
 
 def mark_as_unread(service, query):
     messages_to_mark = search_messages(service, query)
+    if len(messages_to_mark) == 0: # No emails found
+        return print("No emails found")
+    else:
+        print("="*50)
+        for message_id in messages_to_mark:
+            msg = service.users().messages().get(userId='me', id=message_id['id'], format='full').execute()
+            payload = msg['payload']
+            headers = payload.get("headers")
+            if headers:
+                # this section prints email basic info & creates a folder for the email
+                for header in headers:
+                    name = header.get("name")
+                    value = header.get("value")
+                    if name == 'From':
+                        # we print the From address
+                        print("From:", value)
+                    if name == "To":
+                        # we print the To address
+                        print("To:", value)
+                    if name == "Subject":
+                        # we print the Subject
+                        print("Subject:", value)
+                    if name == "Date":
+                        # we print the date when the message was sent
+                        print("Date:", value)
+            print("="*50)
+            print("MARKED AS UNREAD")
     return service.users().messages().batchModify(
         userId='me',
         body={
@@ -27,9 +81,10 @@ if __name__ == "__main__":
     parser.add_argument("-r", "--read", action="store_true", help='Whether to mark the message as read')
     parser.add_argument("-u", "--unread", action="store_true", help='Whether to mark the message as unread')
 
-    args = parser.parse_args()
     service = gmail_authenticate()
+
+    args = parser.parse_args()
     if args.read:
-        mark_as_read(service, args.query)
+        mark_as_read(service, '"' + args.query + '" and label:unread' )
     elif args.unread:
         mark_as_unread(service, args.query)
