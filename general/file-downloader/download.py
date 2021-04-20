28e02a1263f4011dccfc791683e2fdf5ec41edec
x4nth055 <fullclip@protonmail.com>
2021-04-20 14:09:37 +0100

    update file downloader tutorial

--- general/file-downloader/download.py
+++ general/file-downloader/download.py
@@ -1,5 +1,6 @@
 from tqdm import tqdm
 import requests
+import cgi
 import sys
 
 # the url of file you want to download, passed from command line arguments
@@ -11,14 +12,23 @@ response = requests.get(url, stream=True)
 
 # get the total file size
 file_size = int(response.headers.get("Content-Length", 0))
-
-# get the file name
-filename = url.split("/")[-1]
+# get the default filename
+default_filename = url.split("/")[-1]
+# get the content disposition header
+content_disposition = response.headers.get("Content-Disposition")
+if content_disposition:
+    # parse the header using cgi
+    value, params = cgi.parse_header(content_disposition)
+    # extract filename from content disposition
+    filename = params.get("filename", default_filename)
+else:
+    # if content dispotion is not available, just use default from URL
+    filename = default_filename
 
 # progress bar, changing the unit to bytes instead of iteration (default by tqdm)
 progress = tqdm(response.iter_content(buffer_size), f"Downloading {filename}", total=file_size, unit="B", unit_scale=True, unit_divisor=1024)
 with open(filename, "wb") as f:
-    for data in progress:
+    for data in progress.iterable:
         # write data read to the file
         f.write(data)
         # update the progress bar manually
