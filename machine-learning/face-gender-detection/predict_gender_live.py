4e8197b0dadf7015282c920ce37749b8efefe536
x4nth055 <fullclip@protonmail.com>
2021-09-21 13:46:13 +0100

    update gender detection tutorial

--- machine-learning/face-gender-detection/predict_gender_live.py
+++ machine-learning/face-gender-detection/predict_gender_live.py
@@ -59,16 +59,6 @@ def get_faces(frame, confidence_threshold=0.5):
     return faces
 
 
-def display_img(title, img):
-    """Displays an image on screen and maintains the output until the user presses a key"""
-    # Display Image on screen
-    cv2.imshow(title, img)
-    # Mantain output until user presses a key
-    cv2.waitKey(0)
-    # Destroy windows when user presses a key
-    cv2.destroyAllWindows()
-
-
 def get_optimal_font_scale(text, width):
     """Determine the optimal font scale based on the hosting frame width"""
     for scale in reversed(range(0, 60, 1)):
@@ -104,59 +94,64 @@ def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
     return cv2.resize(image, dim, interpolation = inter)
 
 
-def predict_gender(input_path: str):
+def predict_gender():
     """Predict the gender of the faces showing in the image"""
-    # Read Input Image
-    img = cv2.imread(input_path)
-    # resize the image, uncomment if you want to resize the image
-    # img = cv2.resize(img, (frame_width, frame_height))
-    # Take a copy of the initial image and resize it
-    frame = img.copy()
-    if frame.shape[1] > frame_width:
-        frame = image_resize(frame, width=frame_width)
-    # predict the faces
-    faces = get_faces(frame)
-    # Loop over the faces detected
-    # for idx, face in enumerate(faces):
-    for i, (start_x, start_y, end_x, end_y) in enumerate(faces):
-        face_img = frame[start_y: end_y, start_x: end_x]
-        # image --> Input image to preprocess before passing it through our dnn for classification.
-        # scale factor = After performing mean substraction we can optionally scale the image by some factor. (if 1 -> no scaling)
-        # size = The spatial size that the CNN expects. Options are = (224*224, 227*227 or 299*299)
-        # mean = mean substraction values to be substracted from every channel of the image.
-        # swapRB=OpenCV assumes images in BGR whereas the mean is supplied in RGB. To resolve this we set swapRB to True.
-        blob = cv2.dnn.blobFromImage(image=face_img, scalefactor=1.0, size=(
-            227, 227), mean=MODEL_MEAN_VALUES, swapRB=False, crop=False)
-        # Predict Gender
-        gender_net.setInput(blob)
-        gender_preds = gender_net.forward()
-        i = gender_preds[0].argmax()
-        gender = GENDER_LIST[i]
-        gender_confidence_score = gender_preds[0][i]
-        # Draw the box
-        label = "{}-{:.2f}%".format(gender, gender_confidence_score*100)
-        print(label)
-        yPos = start_y - 15
-        while yPos < 15:
-            yPos += 15
-        # get the font scale for this image size
-        optimal_font_scale = get_optimal_font_scale(label,((end_x-start_x)+25))
-        box_color = (255, 0, 0) if gender == "Male" else (147, 20, 255)
-        cv2.rectangle(frame, (start_x, start_y), (end_x, end_y), box_color, 2)
-        # Label processed image
-        cv2.putText(frame, label, (start_x, yPos),
-                    cv2.FONT_HERSHEY_SIMPLEX, optimal_font_scale, box_color, 2)
+    # create a new cam object
+    cap = cv2.VideoCapture(0)
 
-        # Display processed image
-    display_img("Gender Estimator", frame)
-    # uncomment if you want to save the image
-    cv2.imwrite("output.jpg", frame)
-    # Cleanup
+    while True:
+        _, img = cap.read()
+        # resize the image, uncomment if you want to resize the image
+        # img = cv2.resize(img, (frame_width, frame_height))
+        # Take a copy of the initial image and resize it
+        frame = img.copy()
+        if frame.shape[1] > frame_width:
+            frame = image_resize(frame, width=frame_width)
+        # predict the faces
+        faces = get_faces(frame)
+        # Loop over the faces detected
+        # for idx, face in enumerate(faces):
+        for i, (start_x, start_y, end_x, end_y) in enumerate(faces):
+            face_img = frame[start_y: end_y, start_x: end_x]
+            # image --> Input image to preprocess before passing it through our dnn for classification.
+            # scale factor = After performing mean substraction we can optionally scale the image by some factor. (if 1 -> no scaling)
+            # size = The spatial size that the CNN expects. Options are = (224*224, 227*227 or 299*299)
+            # mean = mean substraction values to be substracted from every channel of the image.
+            # swapRB=OpenCV assumes images in BGR whereas the mean is supplied in RGB. To resolve this we set swapRB to True.
+            blob = cv2.dnn.blobFromImage(image=face_img, scalefactor=1.0, size=(
+                227, 227), mean=MODEL_MEAN_VALUES, swapRB=False, crop=False)
+            # Predict Gender
+            gender_net.setInput(blob)
+            gender_preds = gender_net.forward()
+            i = gender_preds[0].argmax()
+            gender = GENDER_LIST[i]
+            gender_confidence_score = gender_preds[0][i]
+            # Draw the box
+            label = "{}-{:.2f}%".format(gender, gender_confidence_score*100)
+            print(label)
+            yPos = start_y - 15
+            while yPos < 15:
+                yPos += 15
+            # get the font scale for this image size
+            optimal_font_scale = get_optimal_font_scale(label,((end_x-start_x)+25))
+            box_color = (255, 0, 0) if gender == "Male" else (147, 20, 255)
+            cv2.rectangle(frame, (start_x, start_y), (end_x, end_y), box_color, 2)
+            # Label processed image
+            cv2.putText(frame, label, (start_x, yPos),
+                        cv2.FONT_HERSHEY_SIMPLEX, optimal_font_scale, box_color, 2)
+
+            # Display processed image
+        
+        # frame = cv2.resize(frame, (frame_height, frame_width))
+        cv2.imshow("Gender Estimator", frame)
+        if cv2.waitKey(1) == ord("q"):
+            break
+        # uncomment if you want to save the image
+        # cv2.imwrite("output.jpg", frame)
+        # Cleanup
     cv2.destroyAllWindows()
 
 
 
 if __name__ == '__main__':
-    # Parsing command line arguments entered by user
-    import sys
-    predict_gender(sys.argv[1])
+    predict_gender()
