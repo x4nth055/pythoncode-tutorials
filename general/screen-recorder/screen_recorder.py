e4135bc755ead604efe58af55d2f579481ed9ccf
x4nth055 <fullclip@protonmail.com>
2021-12-10 10:41:49 +0100

    update screen recorder tutorial

--- general/screen-recorder/screen_recorder.py
+++ general/screen-recorder/screen_recorder.py
@@ -2,14 +2,18 @@ import cv2
 import numpy as np
 import pyautogui
 
-# display screen resolution, get it from your OS settings
-SCREEN_SIZE = (1920, 1080)
+# display screen resolution, get it using pyautogui itself
+SCREEN_SIZE = tuple(pyautogui.size())
 # define the codec
 fourcc = cv2.VideoWriter_fourcc(*"XVID")
+# frames per second
+fps = 12.0
 # create the video write object
-out = cv2.VideoWriter("output.avi", fourcc, 20.0, (SCREEN_SIZE))
+out = cv2.VideoWriter("output.avi", fourcc, fps, (SCREEN_SIZE))
+# the time you want to record in seconds
+record_seconds = 10
 
-while True:
+for i in range(int(record_seconds * fps)):
     # make a screenshot
     img = pyautogui.screenshot()
     # convert these pixels to a proper numpy array to work with OpenCV
