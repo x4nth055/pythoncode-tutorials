e345db72a1830ab321961677316c41f9bc374e57
x4nth055 <fullclip@protonmail.com>
2022-01-18 12:36:29 +0100

    add selects and textareas in form extraction & submission tutorial

--- web-scraping/extract-and-fill-forms/form_submitter.py
+++ web-scraping/extract-and-fill-forms/form_submitter.py
@@ -1,5 +1,4 @@
 from bs4 import BeautifulSoup
-from requests_html import HTMLSession
 
 from pprint import pprint
 from urllib.parse import urljoin
@@ -10,10 +9,18 @@ from form_extractor import get_all_forms, get_form_details, session
 
 # get the URL from the command line
 url = sys.argv[1]
+all_forms = get_all_forms(url)
 # get the first form (edit this as you wish)
-first_form = get_all_forms(url)[0]
+# first_form = get_all_forms(url)[0]
+for i, f in enumerate(all_forms, start=1):
+    form_details = get_form_details(f)
+    print(f"{i} #")
+    pprint(form_details)
+    print("="*50)
+
+choice = int(input("Enter form indice: "))
 # extract all form details
-form_details = get_form_details(first_form)
+form_details = get_form_details(all_forms[choice-1])
 pprint(form_details)
 # the data body we want to submit
 data = {}
@@ -21,14 +28,31 @@ for input_tag in form_details["inputs"]:
     if input_tag["type"] == "hidden":
         # if it's hidden, use the default value
         data[input_tag["name"]] = input_tag["value"]
+    elif input_tag["type"] == "select":
+        for i, option in enumerate(input_tag["values"], start=1):
+            # iterate over available select options
+            if option == input_tag["value"]:
+                print(f"{i} # {option} (default)")
+            else:
+                print(f"{i} # {option}")
+        choice = input(f"Enter the option for the select field '{input_tag['name']}' (1-{i}): ")
+        try:
+            choice = int(choice)
+        except:
+            # choice invalid, take the default
+            value = input_tag["value"]
+        else:
+            value = input_tag["values"][choice-1]
+        data[input_tag["name"]] = value
     elif input_tag["type"] != "submit":
         # all others except submit, prompt the user to set it
         value = input(f"Enter the value of the field '{input_tag['name']}' (type: {input_tag['type']}): ")
         data[input_tag["name"]] = value
+    
 
 # join the url with the action (form request URL)
 url = urljoin(url, form_details["action"])
-
+# pprint(data)
 if form_details["method"] == "post":
     res = session.post(url, data=data)
 elif form_details["method"] == "get":
