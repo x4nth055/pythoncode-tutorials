f3988226ae4fdf9de8e023c9f4dbf98baf04b48f
x4nth055 <fullclip@protonmail.com>
2023-04-17 14:18:15 +0100

    update pdf image extractor

--- web-scraping/pdf-image-extractor/README.md
+++ web-scraping/pdf-image-extractor/README.md
@@ -13,3 +13,19 @@ To run this:
     [!] No images found on page 3
     [!] No images found on page 4
     ```
+- To extract and save all images of 800x800 and higher of `1710.05006.pdf` PDF file, and save them in `images` directory in the PNG format, you run:
+    ```
+    python pdf_image_extractor_cli.py 1710.05006.pdf -o extracted-images -f png -w 800 -he 800
+    ```
+    This will save all available images in the `images` directory and outputs:
+    ```
+    [!] No images found on page 0
+    [+] Found a total of 3 images in page 1
+    [-] Skipping image 1 on page 1 due to its small size.
+    [-] Skipping image 2 on page 1 due to its small size.
+    [-] Skipping image 3 on page 1 due to its small size.
+    [+] Found a total of 3 images in page 2
+    [-] Skipping image 2 on page 2 due to its small size.
+    [!] No images found on page 3
+    [!] No images found on page 4
+    ```
--- web-scraping/pdf-image-extractor/pdf_image_extractor_cli.py
+++ web-scraping/pdf-image-extractor/pdf_image_extractor_cli.py
@@ -2,19 +2,29 @@ import os
 import fitz  # PyMuPDF
 import io
 from PIL import Image
+import argparse
+
+parser = argparse.ArgumentParser(description="Extract images from a PDF file.")
+parser.add_argument("file", help="PDF file to extract images from.")
+parser.add_argument("-o", "--output", help="Output directory for the extracted images.", default="extracted_images")
+parser.add_argument("-f", "--format", help="Desired output image format, default is PNG.", default="png")
+parser.add_argument("-w", "--width", help="Minimum width for extracted images, default is 100.", default=100, type=int)
+parser.add_argument("-he", "--height", help="Minimum height for extracted images, default is 100.", default=100, type=int)
+# Parse the arguments
+args = parser.parse_args()
 
 # Output directory for the extracted images
-output_dir = "extracted_images"
+output_dir = args.output
 # Desired output image format
-output_format = "png"
+output_format = args.format
 # Minimum width and height for extracted images
-min_width = 100
-min_height = 100
+min_width = args.width
+min_height = args.height
 # Create the output directory if it does not exist
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 # File path you want to extract images from
-file = "1710.05006.pdf"
+file = args.file
 # Open the file
 pdf_file = fitz.open(file)
 # Iterate over PDF pages
